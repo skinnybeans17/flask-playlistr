6c98e074d612f7b2ba3f6ef890d864bb74cb7ad9
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-01 15:37:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def video_url_creator(id_lst):
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/')
 def playlists_index():
