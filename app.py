434d563a2e861f00e233fe6945385e71ff33425f
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-01 13:13:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=host)
 db = client.get_default_database()
 playlists = db.playlists
-#comments = db.comments
+comments = db.comments
 
 app = Flask(__name__)
 def video_url_creator(id_lst):
