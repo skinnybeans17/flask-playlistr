cf7226a526a9932887e72508cdae6b4e687ed08b
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-01 11:33:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
+app = Flask(__name__)
+
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=host)
 db = client.get_default_database()
 playlists = db.playlists
 comments = db.comments
-
-app = Flask(__name__)
 
 @app.route('/')
 def index():
