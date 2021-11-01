6d62ed31d35ab64904890959e421c4649d9d4aa9
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-01 13:04:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -98,4 +98,4 @@ def comments_delete(comment_id):
     return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
 
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+  app.run(debug=True)
