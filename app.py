29f1b2de433fa345978af00f637d537bc4d7eef9
skinnybeans17 <89868262+skinnybeans17@users.noreply.github.com>
2021-11-01 13:18:37 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=host)
 db = client.get_default_database()
 playlists = db.playlists
-#comments = db.comments
+comments = db.comments
 
 app = Flask(__name__)
 def video_url_creator(id_lst):
@@ -50,7 +50,7 @@ def playlists_new():
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    #playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
+    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
 @app.route('/playlists/<playlist_id>/edit')
@@ -78,24 +78,24 @@ def playlists_delete(playlist_id):
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
 
-#@app.route('/playlists/comments', methods=['POST'])
-#def comments_new():
-    #"""Submit a new comment."""
-    #comment = {
-        #'title': request.form.get('title'),
-        #'content': request.form.get('content'),
-        #'playlist_id': ObjectId(request.form.get('playlist_id'))
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'playlist_id': ObjectId(request.form.get('playlist_id'))
     }
-    #print(comment)
-    #comment_id = comments.insert_one(comment).inserted_id
-    #return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
-#@app.route('/playlists/comments/<comment_id>', methods=['POST'])
-#def comments_delete(comment_id):
-    #"""Action to delete a comment."""
-    #comment = comments.find_one({'_id': ObjectId(comment_id)})
-    #comments.delete_one({'_id': ObjectId(comment_id)})
-    #return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
+@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+def comments_delete(comment_id):
+    """Action to delete a comment."""
+    comment = comments.find_one({'_id': ObjectId(comment_id)})
+    comments.delete_one({'_id': ObjectId(comment_id)})
+    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
